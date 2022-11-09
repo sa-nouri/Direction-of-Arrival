775117a183e563308d6639f68155c875fe4f3ac3
Salar Nouri <salr.nouri@gmail.com>
2022-11-09 15:59:26 +0330

    Upate Music algorithm

--- Base Models/Music.m
+++ Base Models/Music.m
@@ -1,6 +1,6 @@
 %% MUSIC
 
-function [F_Music] = Music(R,d,Num_Source)
+function [F_Music] = Music(R, d, Num_Source)
     [Betha , lambda ] = eig(R);
     [~ , lambda_index ] = sort(diag(lambda),'descend');
     Betha = Betha(:,lambda_index); 
