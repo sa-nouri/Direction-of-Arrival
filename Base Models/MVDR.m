b54ffe0b357cf851a9096d3d01f50a0c046637da
Salar Nouri <salr.nouri@gmail.com>
2022-11-09 15:59:38 +0330

    Update MVDR

--- Base Models/MVDR.m
+++ Base Models/MVDR.m
@@ -3,12 +3,12 @@
 SNR_dB = 20 ;Tetha = 44;
 Num_Sample = 1e3; Num_Source = 1;  SNR = 100 .^ (SNR_dB/10) ; M = 10 ;d = 0: M-1; 
 
-[R,A] = Covariance (d,Tetha,Num_Source,Num_Sample,SNR_dB);
+[R,A] = Covariance(d, Tetha, Num_Source, Num_Sample, SNR_dB);
 
 angle = 0:0.01:180;
 a = ( 1/sqrt(length(d)) ) * exp( -1i * d' * pi .* cosd(angle));
 F_MVDR = (sum(conj(a) .* (inv(R) * a)))./(sum(conj(a) .* ((inv(R).^2) * a)));
 
-plot(angle,abs(F_MVDR));
+plot(angle, abs(F_MVDR));
 
-%%%
+%%
